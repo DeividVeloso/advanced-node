97d860c870479658f4e0b1cd009a121219d5f148
Deivid Veloso <veloso.deivid@gmail.com>
2018-05-31 14:26:31 -0300

    handle qtd threads in pool libuv

--- 01-event-loop/main/threads.js
+++ 01-event-loop/main/threads.js
@@ -1,3 +1,5 @@
+process.env.UV_THREADPOOL_SIZE = 2;
+
 const crypto = require('crypto');
 
 //Vai exectar a funcao de callback depois de fazer o hash.
@@ -11,4 +13,16 @@ crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
 crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
   //Usado para logar quanto tempo demora para fazer o hash
   console.log('2: ', Date.now() - start);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('3: ', Date.now() - start);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('4: ', Date.now() - start);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+  console.log('5: ', Date.now() - start);
 });
