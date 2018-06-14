30dfbba9d1c0090a9399aeea5b9541f9860ff01d
Deivid Veloso <veloso.deivid@gmail.com>
2018-06-14 09:53:53 -0300

    add fork cluster

--- 01-event-loop/main/index.js
+++ 01-event-loop/main/index.js
@@ -1,15 +1,33 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
+//Estou sendo executado a primeira vez em Master Mode?
+if(cluster.isMaster) {
+  //Cria um child mode e seta o isMaster para false 
+  //e chama o index.js novamente.
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+  cluster.fork();
+} else {
+  //cluster.isMaster é igual a false
+  //Eu sou um child e vou agir como um server
+  const express = require('express');
+  const app = express();
+  
+  app.get('/fast', (req, res) => {
+    res.send('Super fast');
+  });
+  
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('Hi there');
+  });
 
-function doWork(duration) {
-  const start = Date.now();
-  while(Date.now() - start < duration) {}
+  function doWork(duration) {
+    const start = Date.now();
+    while(Date.now() - start < duration) {}
+  }
+  
+  app.listen(3000);
 }
 
-app.get('/', (req, res) => {
-  doWork(5000);
-  res.send('Hi there');
-});
-
-app.listen(3000);
