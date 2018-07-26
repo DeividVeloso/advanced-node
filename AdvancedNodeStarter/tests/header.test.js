c5473d92046576e2e9b1d0970713ff8c706c5845
Deivid Veloso <veloso.deivid@gmail.com>
2018-07-26 09:21:38 -0300

    config puppeteer

--- AdvancedNodeStarter/tests/header.test.js
+++ AdvancedNodeStarter/tests/header.test.js
@@ -1,16 +1,28 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
+
+let browser, page;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    headless: false
+  });
+
+  page = await browser.newPage();
+  await page.goto("http://localhost:3000");
+});
+
+afterEach(async () => {
+  await browser.close();
+});
 
 test("Adds two numbers", () => {
   const sum = 1 + 2;
 
   expect(sum).toEqual(3);
-})
+});
 
-test('We can lauch a browser', async () => {
-  const browser = await puppeteer.launch({
-    headless: false
-  });
+test("We can lauch a browser and see if logo shows up", async () => {
+  const text = await page.$eval("a.brand-logo", el => el.innerHTML);
 
-  const page = await browser.newPage();
-  
-})
+  expect(text).toEqual("Blogster");
+});
