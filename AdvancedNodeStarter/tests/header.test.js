68546f1525e9302e44171ea8f248c28d6aac2433
Deivid Veloso <veloso.deivid@gmail.com>
2018-08-06 09:57:19 -0300

    make test with login and set cookies to auth

--- AdvancedNodeStarter/tests/header.test.js
+++ AdvancedNodeStarter/tests/header.test.js
@@ -34,3 +34,24 @@ test('clicking login starts oauth flow', async () => {
 
   expect(url).toMatch(/accounts\.google\.com/);
 })
+
+test.only('When log in, it should shows logout button', async () => {
+  const id = "5b27a1a0bd6c4016196c0de3"; //UserID
+  const sessionObject = {
+    passport: {
+      user: id
+    }
+  };
+
+  const Buffer = require('safe-buffer').Buffer;
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64'); //Session
+  const Keygrip = require('keygrip');
+  const keys = require('../config/keys');
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign('session=' + sessionString); //Session.sig
+
+  await page.setCookie({name: 'session', value: sessionString})
+  await page.setCookie({name: 'session.sig', value: sig})
+
+  await page.goto('localhost:3000')
+})
