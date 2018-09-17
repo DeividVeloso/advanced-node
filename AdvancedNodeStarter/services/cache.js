9d5aa458d105f098dfc23ffaa5a1b0dc8f3a899d
Deivid Veloso <veloso.deivid@gmail.com>
2018-09-17 10:34:52 -0300

    add git config

--- AdvancedNodeStarter/services/cache.js
+++ AdvancedNodeStarter/services/cache.js
@@ -1,52 +1,51 @@
-const mongoose = require('mongoose');
-const redis = require('redis');
-const util = require('util');
+const mongoose = require("mongoose");
+const redis = require("redis");
+const util = require("util");
+const keys = require("../config/keys");
 
-const redisUrl = 'redis://127.0.0.1:6379';
-const client = redis.createClient(redisUrl);
+const client = redis.createClient(keys.redisUrl);
 
 client.hget = util.promisify(client.hget);
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.cache = function (option = {}) {
+mongoose.Query.prototype.cache = function(option = {}) {
   this.useCache = true;
-  this.hashKey = JSON.stringify(option.key || '') ;
+  this.hashKey = JSON.stringify(option.key || "");
 
+  return this; //Para deixar chaining colocar outras funcoes depois do cache se quiser ex: .limt(1)
+};
 
-  return this;//Para deixar chaining colocar outras funcoes depois do cache se quiser ex: .limt(1)
-}
-
-mongoose.Query.prototype.exec = async function () {
+mongoose.Query.prototype.exec = async function() {
   if (!this.useCache) {
     return await exec.apply(this, arguments);
   }
 
-  const key = JSON.stringify(Object.assign({}, this.getQuery, {
-    collection: this.mongooseCollection.name
-  }))
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery, {
+      collection: this.mongooseCollection.name
+    })
+  );
 
-  const cacheValue = await client.hget(this.hashKey, key)
+  const cacheValue = await client.hget(this.hashKey, key);
   if (cacheValue) {
-    console.log("PEgou do Cache")
-    const doc = JSON.parse(cacheValue)
+    console.log("PEgou do Cache");
+    const doc = JSON.parse(cacheValue);
 
     return Array.isArray(doc)
       ? doc.map(d => new this.model(d))
-      : new this.model(doc)
-
+      : new this.model(doc);
   }
-  console.log("NAO PEgou do Cache")
+  console.log("NAO PEgou do Cache");
   const result = await exec.apply(this, arguments);
-  console.log("OPS", this.hashKey, key)
-  client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
+  console.log("OPS", this.hashKey, key);
+  client.hset(this.hashKey, key, JSON.stringify(result), "EX", 10);
 
   return result;
-
-}
+};
 
 module.exports = {
   clearHash(hashKey) {
-    client.del(JSON.stringify(hashKey))
+    client.del(JSON.stringify(hashKey));
   }
-}
+};
