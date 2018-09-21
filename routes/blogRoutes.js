1818f6580cadb2f26c670ee2a6e87302cb02ee6b
Deivid Veloso <veloso.deivid@gmail.com>
2018-09-21 14:19:41 -0300

    add image uploads

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
-const cleanCache = require('../middlewares/cleanCache');
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
+const cleanCache = require("../middlewares/cleanCache");
 
-const Blog = mongoose.model('Blog');
+const Blog = mongoose.model("Blog");
 
 module.exports = app => {
-
-  app.post('/api/blogs', requireLogin, cleanCache, async (req, res) => {
-    const { title, content } = req.body;
+  app.post("/api/blogs", requireLogin, cleanCache, async (req, res) => {
+    const { title, content, imageUrl } = req.body;
 
     const blog = new Blog({
       title,
       content,
+      imageUrl,
       _user: req.user.id
     });
 
@@ -23,7 +23,7 @@ module.exports = app => {
     }
   });
 
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+  app.get("/api/blogs/:id", requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
       _id: req.params.id
@@ -32,12 +32,10 @@ module.exports = app => {
     res.send(blog);
   });
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog
-      .find({ _user: req.user.id })
-      .cache({ key: req.user.id });
+  app.get("/api/blogs", requireLogin, async (req, res) => {
+    const blogs = await Blog.find({ _user: req.user.id }).cache({
+      key: req.user.id
+    });
     res.send(blogs);
   });
-
-
 };
